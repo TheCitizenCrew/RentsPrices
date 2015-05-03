ec3e0ab4807588d1d786e743de3c8068fe8a2527
Cyrille Giquello <cyrille@giquello.fr>
2015-05-03 19:44:02 +0200

    typo

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -1,62 +1,84 @@
-<?php namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input ;
-use DB ;
+use Illuminate\Support\Facades\Input;
+use DB;
 
 class RentController extends BaseController
 {
 
+	public function editNew()
+	{
+		return $this->edit( null );
+	}
+
 	/**
 	 * Edit a new or an existing Rent.
-	 * 
-	 * @param string $id
+	 *
+	 * @param string $id        	
 	 * @return \Illuminate\View\View
 	 */
-    public function edit($id=null)
-    {
-    	if( $id==null)
-    	{
-    		$rent = new \App\Models\Rent();
-    	}
-    	else
-    	{
-    		$rent = \App\Models\Rent::find($id);
-    	}
-    	return view('rentEdit', ['rent'=>$rent]);
-    }
+	public function edit( $id )
+	{
+		if( $id == null )
+		{
+			$rent = new \App\Models\Rent();
+		}
+		else
+		{
+			$rent = \App\Models\Rent::findOrFail( $id );
+		}
+		// error_log(var_export($rent, true));
+		return view( 'rentEdit', [ 'rent' => $rent ] );
+	}
 
-    /**
-     * Save a Rent and it's RentPrice(s).
-     * 
-     * @param Request $request
-     * @return \Illuminate\View\View
-     */
-    public function save(Request $request)
-    {
-    	$rent = new \App\Models\Rent();
+	/**
+	 * Save a Rent and it's RentPrice(s).
+	 *
+	 * @param Request $request        	
+	 * @return \Illuminate\View\View
+	 */
+	public function save( Request $request )
+	{
+		return $this->update( $request, null );
+	}
 
-    	DB::transaction(function() use($request){
-    	
-    		// Rent
-     		$this->validate($request, \App\Models\Rent::$rules);
-     		$rent = \App\Models\Rent::create($request->all() );
-
-     		$rpos=array();
-     		// RentPrices
-     		foreach( $request->get('rentprice') as $rp )
-     		{
-     			$validator = $this->getValidationFactory()->make($rp, \App\Models\RentPrice::$rules );
-     			if ($validator->fails()) {
-     				$this->throwValidationException($request, $validator);
-     			}
-     			$rpos[] = new \App\Models\RentPrice($rp);
-     		}
-     		$rent->prices()->saveMany($rpos);
-    	});
-
-    	return view('rentEdit', ['rent'=>$rent]);
-    }
-
+	public function update( Request $request, $id )
+	{
+		DB::transaction( 
+			function () use($request )
+			{
+				
+				// Rent
+				$this->validate( $request, \App\Models\Rent::$rules );
+				if( $id == null )
+				{
+					$rent = \App\Models\Rent::create( $request->all() );
+				}
+				else
+				{
+					$rent = \App\Models\Rent::findOrFail( $id );
+					$rent->fill( $request->all() );
+				}
+				
+				$rpos = array ();
+				// RentPrices
+				foreach( $request->get( 'rentprice' ) as $rp )
+				{
+					$validator = $this->getValidationFactory()->make( $rp, 
+						\App\Models\RentPrice::$rules );
+					if( $validator->fails() )
+					{
+						$this->throwValidationException( $request, $validator );
+					}
+					$rpos[] = new \App\Models\RentPrice( $rp );
+				}
+				$rent->prices()->saveMany( $rpos );
+			} );
+		
+		return view( 'rentEdit', [ 'rent' => $rent ] );
+	}
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,53 +1,50 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
- * Http Method Spoofing
- * 
-	<form action="/foo/bar" method="POST">
-	<input type="hidden" name="_method" value="PUT">
-	<input type="hidden" name="_token" value="{{ csrf_token() }}">
-	</form>
+ * |--------------------------------------------------------------------------
+ * | Application Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register all of the routes for an application.
+ * | It's a breeze. Simply tell Laravel the URIs it should respond to
+ * | and give it the controller to call when that URI is requested.
+ * |
+ *
+ * Http Method Spoofing :
+ * <form action="/foo/bar" method="POST">
+ * <input type="hidden" name="_method" value="PUT">
+ * <input type="hidden" name="_token" value="{{ csrf_token() }}">
+ * </form>
+ */
+$app->get( '/', 'App\Http\Controllers\Controller@home' );
 
-*/
-
-$app->get('/', function() use ($app) {
-    return $app->welcome();
-});
-
-$app->get('/', 'App\Http\Controllers\Controller@home');
-
-$app->group(['prefix' => 'rent'], function($app)
-{
-	$app->get('', 'App\Http\Controllers\RentController@edit');
-	$app->get('{id:[0-9]+}', 'App\Http\Controllers\RentController@edit');
-	$app->post('', 'App\Http\Controllers\RentController@save');
-});
+$app->group( [ 'prefix' => 'rent' ], 
+	function ( $app )
+	{
+		$app->get( '', 'App\Http\Controllers\RentController@editNew' );
+		$app->get( '/{id:[0-9]+}', 'App\Http\Controllers\RentController@edit' );
+		$app->post( '', 'App\Http\Controllers\RentController@save' );
+		$app->post( '/{id:[0-9]+}', 'App\Http\Controllers\RentController@update' );
+	} );
 
 /**
  * REST API
  * http://en.wikipedia.org/wiki/Representational_state_transfer
  * http://rest.elkstein.org/
  */
-$app->group(['prefix' => 'api'], function($app)
-{
-	/**
-	 * URIs for Rents
-	 */
-	$app->group(['prefix' => 'rents'], function($app)
+$app->group( [ 'prefix' => 'api' ], 
+	function ( $app )
 	{
-		$app->get('', 'App\Http\Controllers\Api\RentsController@find');
-		$app->post('', 'App\Http\Controllers\Api\RentsController@create');
-		$app->get('{id:[0-9]+}', 'App\Http\Controllers\Api\RentsController@get');
-		$app->put('{id:[0-9]+}', 'App\Http\Controllers\Api\RentsController@save');
-		$app->delete('{id:[0-9]+}', 'App\Http\Controllers\Api\RentsController@delete');
-
-	});
-});
+		/**
+		 * URIs for Rents
+		 */
+		$app->group( [ 'prefix' => 'rents' ], 
+			function ( $app )
+			{
+				$app->get( '', 'App\Http\Controllers\Api\RentsController@find' );
+				$app->post( '', 'App\Http\Controllers\Api\RentsController@create' );
+				$app->get( '{id:[0-9]+}', 'App\Http\Controllers\Api\RentsController@get' );
+				$app->put( '{id:[0-9]+}', 'App\Http\Controllers\Api\RentsController@save' );
+				$app->delete( '{id:[0-9]+}', 'App\Http\Controllers\Api\RentsController@delete' );
+			} );
+	} );
