a037a461c2d36fc2197197006df1242dad9615e2
Cyrille Giquello <cyrille@giquello.fr>
2015-05-10 20:35:40 +0200

    data seeds

--- database/seeds/RentsTableSeeder.php
+++ database/seeds/RentsTableSeeder.php
@@ -10,23 +10,34 @@ class RentsTableSeeder extends Seeder {
 		DB::table('rents')->delete();
 
 		$rt = \App\Models\Rent::create([
-				'buildingIndividual' => 0,
-				'buildingStage' => 2,
-				'buildingName' => '',
-				'street' => '29 quai Paul Bert',
-				'zipcode' => '37100',
-				'city' => 'Tours',
+			'buildingIndividual' => 0,
+			'buildingStage' => 2,
+			'buildingName' => '',
+			'street' => '29 quai Paul Bert',
+			'zipcode' => '37100',
+			'city' => 'Tours',
+			'country' => 'France',
+			'addrLat' => 47.4019148782551,
+			'addrLng' => 0.68988561630249
 		]);
 
 		$rp = \App\Models\RentPrice::create([
-				'year' => '2014',
-				'price'=>500,
-				'rent_id'=>$rt->id
+			'year' => '2014', 'price'=>500, 'rent_id'=>$rt->id
 		]);
 		$rp = \App\Models\RentPrice::create([
-				'year' => '2015',
-				'price'=>500,
-				'rent_id'=>$rt->id
+			'year' => '2015', 'price'=>500, 'rent_id'=>$rt->id
+		]);
+
+		$rt = \App\Models\Rent::create([
+			'buildingIndividual' => 0,
+			'buildingStage' => 4,
+			'buildingName' => '',
+			'street' => '74 Rue Philippe de Girard',
+			'zipcode' => '75018',
+			'city' => 'Tours',
+			'country' => 'France',
+			'addrLat' => 48.8883675,
+			'addrLng' => 2.360674
 		]);
 
 	}
