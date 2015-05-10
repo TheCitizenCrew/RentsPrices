40929d753f4f7f848e8ca070a34d62282f41cad0
Cyrille Giquello <cyrille@giquello.fr>
2015-05-10 17:34:10 +0200

    add LICENSE and About

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+
+@extends('layout')
+
+@section('title', 'Ajouter un loyer')
+
+@section('content')
+<h1>À propos</h1>
+
+<p>Cette application permet de glaner les prix des loyers.</p>
+
+<p>Le code source, <a href="https://github.com/TheCitizenCrew/RentsPrices">déposé sur GitHub</a>, est partagé via la licence <a href="http://www.gnu.org/licenses/gpl-3.0.txt">GPLv3</a> (GNU GENERAL PUBLIC LICENSE version 3), c'est à dire que tout travail dérivé devra être partagé selon les mêmes conditions.</p>
+
+@stop
