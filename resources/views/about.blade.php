2b65900d827ca8e0df14c26c1a6768bf1c47c101
Cyrille Giquello <cyrille@giquello.fr>
2015-05-13 13:19:25 +0200

    add licenses and others rights

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,10 +6,64 @@
 @section('content')
 <h1>À propos</h1>
 
-<p>Cette application permet de glaner les prix des loyers.</p>
+<p>Cette application permet de récolter et partager les prix des loyers.</p>
 
-<p>Le code source, <a href="https://github.com/TheCitizenCrew/RentsPrices">déposé sur GitHub</a>, est partagé via la licence <a href="http://www.gnu.org/licenses/gpl-3.0.txt">GPLv3</a> (GNU GENERAL PUBLIC LICENSE version 3), c'est à dire que tout travail dérivé devra être partagé selon les mêmes conditions.</p>
+<h2>Mentions Légales</h2>
 
-<p>Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></p>
+<h3>Droits d'utilisation et de distribution</h3>
+
+<p>Les contenus, les données et le programme constituant ce service de récolte des prix des loyers sont partagés et protégés selon ces termes :</p>
+<ul>
+<li><img src="https://s.yimg.com/pw/images/cc_icon_attribution.gif.v2" /> <strong>Paternité</strong>: vous devez citer l'auteur originale: The Citizen Crew Contributors.</li>
+<li><img src="https://s.yimg.com/pw/images/cc_icon_sharealike.gif.v2" /> <strong>Partage selon les Conditions Initiales</strong>: vous pouvez re-distribution ces contenus, données et programme orginaux ou modifiés sous un contrat identique à celui-ci.</li>
+</ul>
+
+<p>En utilisant les outils juridiques que sont les licences suivantes :</p>
+
+<div class="panel panel-default">
+  <div class="panel-heading">Les contenus</div>
+  <div class="panel-body">
+		<p>Les contenus de cette application sont partagées et protégées par 
+		<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+		Licence Creative Commons Attribution -  Partage dans les Mêmes Conditions 4.0 International</a> <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+		<img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>.
+		</p>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-heading">Les données</div>
+  <div class="panel-body">
+		<p>Les données récoltées et stockées par cette application sont partagées et protégées par
+		<a rel="license" href="http://opendatacommons.org/licenses/odbl/">
+		 Open Database License</a> <a rel="license" href="http://opendatacommons.org/licenses/odbl/">
+		<img alt="Open Database License" style="border-width:0" height="36px" src="/img/odbl_logo.png" /></a>.
+		</p>
+  </div>
+</div>
+
+
+<div class="panel panel-default">
+  <div class="panel-heading">Le programme</div>
+  <div class="panel-body">
+		<p>Le programme de cette application est partagé et protégé par
+		<a rel="license" href="http://www.gnu.org/licenses/gpl-3.0.txt">
+		 GNU GENERAL PUBLIC LICENSE version 3</a> <a rel="license" href="http://www.gnu.org/licenses/gpl-3.0.txt">
+		<img alt="GNU GENERAL PUBLIC LICENSE version 3" style="border-width:0" src="https://www.gnu.org/graphics/gplv3-127x51.png" height="36px" /></a>.
+		</p>
+		<p>Le code source est disponible sur GitHub : <a href="https://github.com/TheCitizenCrew/RentsPrices">github.com/TheCitizenCrew/RentsPrices</a>.
+		</p>		
+  </div>
+</div>
+
+<h3>Autres ayants droit</h3>
+
+<div class="panel panel-default">
+  <div class="panel-heading">Le fond de carte</div>
+  <div class="panel-body">
+		<p>Les tuiles composant le fond de carte sont mises gracieusement à disposition par la société <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></p>
+  </div>
+</div>
+
 
 @stop
