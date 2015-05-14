c3ec3cc9bed062943438dcff9570537468966ed8
Cyrille Giquello <cyrille@giquello.fr>
2015-05-14 13:17:36 +0200

    rent map marker placement offset

--- public/js/RentsMap.common.js
+++ public/js/RentsMap.common.js
@@ -3,14 +3,16 @@
 var iconGeolocAuto = new L.Icon({
 	iconUrl: '/img/marker-leaf-green2.png',
 	iconSize: new L.Point(64, 64),
+	iconAnchor: [10, 62],
 	shadowUrl: '/img/marker-leaf-shadow.png',
 	shadowSize: [50, 44],
-  shadowAnchor: [22, 12]
+	shadowAnchor: [0, 48]
 });
 var iconGeolocManual = new L.Icon({
 	iconUrl: '/img/marker-leaf-yellow2.png',
 	iconSize: new L.Point(64, 64),
+	iconAnchor: [10, 62],
 	shadowUrl: '/img/marker-leaf-shadow.png',
 	shadowSize: [50, 44],
-	shadowAnchor: [22, 12]
+	shadowAnchor: [0, 48]
 });
